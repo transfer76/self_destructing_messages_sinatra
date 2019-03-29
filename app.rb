45193249440f5a0ed97e160b33646f437de49de8
transfer76 <shina76@ukr.net>
2019-03-30 01:19:29 +0200

    Add new methods

--- app.rb
+++ app.rb
@@ -10,10 +10,21 @@ class Message < ActiveRecord::Base
 
 end
 
-  get '/' do
-    erb :new
-  end
+get '/' do
+  @messages = Message.all
+  erb :new
+end
 
-  post "message/create" do
-
+post '/message/create' do
+  message = Message.new(text: params['text'], urlsafe: SecureRandom.urlsafe_base64)
+  if params['destruction_option'] == 'link_visit'
+    message.visits_remaining = 1
   end
+  message.save
+  redirect to("/message/#{message.urlsafe}")
+end
+
+get '/message/:urlsafe' do
+  @message = Message.where(urlsafe: params['urlsafe']).last
+  erb :show
+end
