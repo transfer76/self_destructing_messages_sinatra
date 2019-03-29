9a4ff4135c62e16c3b8aac9b046ac724cc289577
transfer76 <shina76@ukr.net>
2019-03-30 01:33:03 +0200

    Add class Message

--- app.rb
+++ app.rb
@@ -3,12 +3,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'aes'
+require 'message'
 
 set :database, "sqlite3:message.db"
-
-class Message < ActiveRecord::Base
-
-end
 
 get '/' do
   @messages = Message.all
@@ -18,7 +15,12 @@ end
 post '/message/create' do
   message = Message.new(text: params['text'], urlsafe: SecureRandom.urlsafe_base64)
   if params['destruction_option'] == 'link_visit'
-    message.visits_remaining = 1
+    message.visits_remaining = 1 + 1
+  else
+    Thread.new do
+      sleep 1.hour
+      message.delete
+    end
   end
   message.save
   redirect to("/message/#{message.urlsafe}")
@@ -26,5 +28,16 @@ end
 
 get '/message/:urlsafe' do
   @message = Message.where(urlsafe: params['urlsafe']).last
+  if @message.nil?
+    redirect to('/')
+  else
+    if @message.destroyed_via_link_visits?
+      @message.visits_remaining -= 1
+      @message.save
+      if @message.visits_remaining == 0
+        @message.delete
+      end
+    end
+  end
   erb :show
 end
---
+++ lib/message.rb
@@ -0,0 +1,16 @@
+class Message < ActiveRecord::Base
+  def destroyed_after_time?
+    visits_remaining.nil?
+  end
+
+  def destroyed_via_link_visits?
+    !destroyed_after_time?
+  end
+
+  def time_remaining
+    if destroyed_after_time?
+      seconds = 1.hour - (Time.now - created_at)
+      Time.at(seconds).strftime('%H:%M:%S')
+    end
+  end
+end
