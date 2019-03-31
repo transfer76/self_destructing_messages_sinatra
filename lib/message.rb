b970bad6d41ad06231ec5bf3ca5bb8c0c690307c
transfer76 <shina76@ukr.net>
2019-03-31 17:20:15 +0300

    Change time remaining for heroku

--- lib/message.rb
+++ lib/message.rb
@@ -9,7 +9,7 @@ class Message < ActiveRecord::Base
 
   def time_remaining
     if destroyed_after_time?
-      time_left = -2.hour - (Time.now - created_at)
+      time_left = 1.hour - (Time.now - created_at)
       Time.at(time_left).strftime('%H:%M:%S')
     end
   end
